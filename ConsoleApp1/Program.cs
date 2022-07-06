577564305675d6465ef54408b0131c40a6ad203c
Matah <Matah@DESKTOP-5VBIVJF>
2022-07-06 16:10:03 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Numeros pares de 1 a 100");
+
+                for (int i = 0; i <= 100; i += 2)
+
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine($"Number: {i}");
+
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
